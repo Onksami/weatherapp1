0be21e1e79b89269c7109818ff66215a43a0f042
irfan önk <57913715+irfanonk@users.noreply.github.com>
2022-09-17 16:50:51 +0300

    change dynamicly

--- app.js
+++ app.js
@@ -1,5 +1,80 @@
 const dt = new Date();
-document.getElementById("date").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ 
-(("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ 
-(("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
+document.getElementById("date").innerHTML =
+  ("0" + dt.getDate()).slice(-2) +
+  "." +
+  ("0" + (dt.getMonth() + 1)).slice(-2) +
+  "." +
+  dt.getFullYear() +
+  " " +
+  ("0" + dt.getHours()).slice(-2) +
+  ":" +
+  ("0" + dt.getMinutes()).slice(-2);
 
+const daysRow = document.getElementById("week-days");
+
+const cities = [{ name: "Ankara" }, { name: "İstanbul" }];
+
+const citySelect = document.getElementById("cities");
+for (let index = 0; index < cities.length; index++) {
+  const element = cities[index];
+  citySelect.options[citySelect.options.length] = new Option(
+    element.name,
+    element.name
+  );
+}
+
+const daysWeather = [
+  {
+    title: "Monday",
+    degree: 24,
+    condition: "Rainy",
+  },
+  {
+    title: "Tuesday",
+    degree: 30,
+    condition: "Sunny",
+  },
+
+  {
+    title: "Wednesday",
+    degree: 25,
+    condition: "Cloudy",
+  },
+];
+daysWeather.forEach((element) => {
+  const { condition, title, degree } = element;
+  const div = `
+                <div class="days">
+              <ul class="daysItems">
+                <li>${condition}</li>
+                <li class="daysB">${title} </li>
+                <li>${degree} </li>
+              </ul>
+            </div>
+    `;
+  daysRow.innerHTML += div;
+});
+
+// const daysItems = document.getElementsByClassName("daysItems");
+const daysItems = document.getElementsByClassName("daysItems");
+
+for (let index = 0; index < daysItems.length; index++) {
+  const element = daysItems[index];
+  element.addEventListener("click", (e) => {
+    if (e.target.classList.value === "daysB") {
+      console.log("e", e.target.innerHTML);
+      const selectedDay = daysWeather.filter(
+        (elm) => elm.title === e.target.innerHTML.replace(" ", "")
+      );
+      console.log("selectedDay", selectedDay[0]);
+      document.getElementById("degree").innerHTML = selectedDay[0].degree;
+      document.getElementById("condition").innerHTML = selectedDay[0].condition;
+    }
+  });
+}
+
+// daysItems.forEach(function (element) {
+//   element.addEventListener("click", (e) => {
+//     console.log("e", e.target.innerHTML);
+//   });
+// });
